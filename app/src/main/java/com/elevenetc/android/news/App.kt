586807ef2c56119f5a5d3f60085ece7a04840def
“Eugene <shumvlesu@gmail.com>
2020-06-08 00:58:34 +0200

    DI cleanup

--- app/src/main/java/com/elevenetc/android/news/App.kt
+++ app/src/main/java/com/elevenetc/android/news/App.kt
@@ -1,12 +1,21 @@
 package com.elevenetc.android.news
 
 import android.app.Application
+import com.elevenetc.android.news.core.di.AppComponent
 import com.elevenetc.android.news.core.di.AppModule
 import com.elevenetc.android.news.core.di.DaggerAppComponent
 
 class App : Application() {
-    val appComponent = DaggerAppComponent
-        .builder()
-        .appModule(AppModule(this))
-        .build()
+
+    override fun onCreate() {
+        super.onCreate()
+        appComponent = DaggerAppComponent
+            .builder()
+            .appModule(AppModule(this))
+            .build()
+    }
+
+    companion object {
+        lateinit var appComponent: AppComponent
+    }
 }
--- app/src/main/java/com/elevenetc/android/news/core/BaseFragment.kt
+++ app/src/main/java/com/elevenetc/android/news/core/BaseFragment.kt
@@ -3,23 +3,7 @@ package com.elevenetc.android.news.core
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
 import com.elevenetc.android.news.App
-import com.elevenetc.android.news.core.di.AppComponent
-import io.reactivex.disposables.CompositeDisposable
 
 open class BaseFragment(@LayoutRes val contentLayoutId: Int) : Fragment(contentLayoutId) {
-    /**
-     * Available after [Fragment.onAttach] is called
-     */
-    val appComponent: AppComponent by lazy { (context!!.applicationContext as App).appComponent }
-
-    protected var subs = CompositeDisposable()
-
-    override fun onDestroyView() {
-        subs.dispose()
-        super.onDestroyView()
-    }
-
-    fun showRetry() {
-
-    }
+    val appComponent = App.appComponent
 }
